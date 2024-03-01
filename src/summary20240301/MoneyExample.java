3f7be53f80a0b64373611795f58dabfa2ae21aa3
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-03-01 09:46:35 +0100

    Money example

---
+++ src/summary20240301/MoneyExample.java
@@ -0,0 +1,20 @@
+package summary20240301;
+
+public class MoneyExample {
+
+    public static void main(String[] args) {
+
+        double sum = 0.0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += 0.1;
+        }
+
+        System.out.println(sum == 1.0);
+        System.out.println(sum);
+
+
+    }
+
+
+}
