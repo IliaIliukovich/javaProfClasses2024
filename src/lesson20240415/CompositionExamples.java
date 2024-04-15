53e790999afb8719b0f8e8a22eaabb3b940989f7
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-04-15 10:38:38 +0200

    Functional interfaces

---
+++ src/lesson20240415/CompositionExamples.java
@@ -0,0 +1,53 @@
+package lesson20240415;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class CompositionExamples {
+
+    public static void main(String[] args) {
+        Consumer<Integer> consumer = (integer -> System.out.println(integer));
+        Consumer<Integer> comositeConsumer = consumer
+                .andThen(integer -> System.out.println(integer + 10))
+                .andThen(integer -> System.out.println(integer + 20));
+
+        comositeConsumer.accept(10);
+        comositeConsumer.accept(99);
+
+        Predicate<String> isLongEnough = (s) -> s.length() > 3;
+        Predicate<String> startsWithS = (s) -> s.startsWith("S");
+        Predicate<String> compositePredicate = isLongEnough.negate().and(startsWithS);
+
+        System.out.println(compositePredicate.test("Hello"));
+        System.out.println(compositePredicate.test("Summer"));
+        System.out.println(compositePredicate.test("Sum"));
+
+
+        Function<String, String> function1 = s -> s + " 1";
+        Function<String, String> function2 = s -> s + " 2";
+        Function<String, String> function3 = s -> s + " 3";
+        Function<String, String> compositeFunction = function1.andThen(function2).andThen(function3);
+
+        String result = compositeFunction.apply("Test data");
+        System.out.println(result);
+
+        compositeFunction = function1.compose(function2).compose(function3);
+        result = compositeFunction.apply("Test data");
+        System.out.println(result);
+
+        // Создать три функции:
+        //- обрезает входную строку до 11 символов,
+        //- добавляет текст "Info: " перед
+        //- делает основной текст upper case
+        //Составить композитную функцию так, чтобы:
+        //Input:
+        //"Some random data"
+        //Output:
+        //"Info: SOME RANDOM
+
+    }
+
+
+
+}
---
+++ src/lesson20240415/LambdaScoups.java
@@ -0,0 +1,51 @@
+package lesson20240415;
+
+import lesson20240313.anonymousclass.Playable;
+
+import java.util.function.Consumer;
+
+public class LambdaScoups {
+
+    public static void main(String[] args) {
+
+        String data = "Main Data";
+        int counter = 0; // must be effectively final
+//        counter++;
+
+        Playable footballPlayer = new Playable() {
+            String data = "Anonymous Data"; // shadowing
+
+            @Override
+            public void play() {
+//                String data = "Anonymous Data"; // shadowing
+                System.out.println(data);
+                System.out.println(counter);
+            }
+        };
+
+        footballPlayer.play();
+
+        Playable chessPlayer = () -> {
+//          String data = "Lambda Data"; // impossible
+          System.out.println(data);
+          System.out.println(counter);
+        };
+
+        chessPlayer.play();
+
+        Consumer<Integer> consumer = (integer) -> {
+            if (integer % 2 == 0) {
+                System.out.println(integer);
+            } else {
+                System.err.println(integer);
+            }
+        };
+
+        consumer.accept(2);
+        consumer.accept(3);
+        consumer.accept(4);
+        consumer.accept(5);
+    }
+
+
+}
