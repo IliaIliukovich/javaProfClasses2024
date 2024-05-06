cf4374383dbde148fd1d52f11cad1078c08bc914
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-05-06 12:55:01 +0200

    Exceptions, read-write from file

---
+++ src/lesson20240506/io/Task.java
@@ -0,0 +1,28 @@
+package lesson20240506.io;
+
+import java.io.*;
+
+public class Task {
+
+    public static void main(String[] args) {
+
+        // Записать 3 бита '101' в файл, считать их.
+        File file = new File("resources/binaryData");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        byte b = 5;
+        byte b = 0b101;
+
+        try (OutputStream stream = new FileOutputStream(file, true)) {
+//            stream.write(new byte[]{b});
+            stream.write((int) b);
+        } catch (IOException ignore) {
+        }
+
+
+    }
+}
