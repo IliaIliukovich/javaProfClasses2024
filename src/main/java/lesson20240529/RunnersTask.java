82a906fea929ef8ed2ade669d9b6740223006537
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-05-29 12:18:34 +0200

    synchronization

---
+++ src/main/java/lesson20240529/RunnersTask.java
@@ -0,0 +1,23 @@
+package lesson20240529;
+
+public class RunnersTask {
+
+    // Спортивное соревнование. Бегут 3 бегуна. Каждый пробегает дистанцию за случайное время.
+    // Каждый бегун земеряет свой результат и сообщает его тренеру. В конце забега выводится информация о победителе.
+    // Реализовать данную модель с помощью запуска отдельных потоков. Бегуна представить через класс Runner implements Runnable.
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Runner implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
+
+}
