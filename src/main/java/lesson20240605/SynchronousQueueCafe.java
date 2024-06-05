258135371f3f6f1312e7a6a219ab2702171b7d53
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-06-05 10:54:04 +0200

    SynchronousQueue example

---
+++ src/main/java/lesson20240605/SynchronousQueueCafe.java
@@ -0,0 +1,65 @@
+package lesson20240605;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class SynchronousQueueCafe {
+    private static SynchronousQueue<String> orderQueue = new SynchronousQueue<>(true);
+    private static SynchronousQueue<String> readyCourseQueue = new SynchronousQueue<>(true);
+
+    static class Visitor implements Runnable {
+
+        String order;
+
+        public Visitor(String order) {
+            this.order = order;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " comes into cafe");
+            try {
+                orderQueue.put(order);
+                System.out.println(Thread.currentThread().getName() + " made an order");
+                System.out.println(Thread.currentThread().getName() + " waits ...");
+                String readyCourse = readyCourseQueue.take();
+                System.out.println(Thread.currentThread().getName() + " eats/drinks: " + readyCourse);
+                Thread.sleep(5000);
+                System.out.println(Thread.currentThread().getName() + " says goodbye and leaves the cafe");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Host implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("Host waits for visitors...");
+                try {
+                    String order = orderQueue.take();
+                    System.out.println("Host got an order from visitor and started cooking");
+                    Thread.sleep(10000);
+                    System.out.println("Host cooked the order");
+                    readyCourseQueue.put("Cooked: " + order);
+                    System.out.println("Host gave meal to the visitor");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Host()).start();
+        Thread.sleep(5000);
+
+        new Thread(new Visitor("Pizza"), "Tom").start();
+        new Thread(new Visitor("Cafe"), "Jane").start();
+        new Thread(new Visitor("Pasta"), "Mark").start();
+    }
+
+}
